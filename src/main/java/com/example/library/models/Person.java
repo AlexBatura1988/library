13839455be14ef1a25af96c0add872af0a275538
AlexBatura1988 <alex.batura88@gmail.com>
2022-11-01 18:43:10 +0200

    add model Person and index.html

---
+++ src/main/java/com/example/library/models/Person.java
@@ -0,0 +1,47 @@
+package com.example.library.models;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Person {
+    private int id;
+    @NotEmpty(message = "please enter a name")
+    @Size(min = 2,max = 50, message ="name must be for 2 to 50 digits")
+    private String fullName;
+    @Min(value = 1900, message = "year of birth must be greater than 1900")
+    private int yearOfBirth;
+
+    public Person(){
+
+    }
+
+    public Person(String fullName, int yearOfBirth) {
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+}
